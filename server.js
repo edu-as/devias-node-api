9651ec6eb72430c315e48daeaa4c6741e89c6425
edu-as <edu-as@hotmail.com>
2020-09-22 10:17:19 -0300

    Mudando nome da aplicação.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+'use strict'
 const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res)=> {
-    res.send('Hello Api-node-Duzao');
+    res.send('Hello Wecome to Devias-node-api developed by Duzão');
 });
 
 app.listen(3001);
