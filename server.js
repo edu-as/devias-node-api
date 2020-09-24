ce11ee67bdbe74bc75e31f19c3ab1276cd922655
edu-as <edu-as@hotmail.com>
2020-09-23 23:42:14 -0300

    instalando nodemon e mongodb, e configurando db

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-'use strict'
+// node api Rocketseat
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser:true }
+);
+
+//primeira rota
 app.get('/', (req, res)=> {
     res.send('Hello Wecome to Devias-node-api developed by Duzão');
 });
 
 app.listen(3001);
+
+
+/* CURSO NODE BALTA.IO
+
+'use strict'
+
+const http = require ('http');
+const express = require('express');
+const debug = require('debug')('deviasApiNode:serve');
+
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();*/
